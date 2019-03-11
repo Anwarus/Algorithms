2b4009264f2f263496d123d62632e768fdea9011
Anwarus <Anwarus1289@gmail.com>
2019-03-11 22:55:41 +0100

    Add algorithm description and improve navbar

--- js/algorithms.js
+++ js/algorithms.js
@@ -1,8 +1,21 @@
 const algorithm = {
     linearSearch: function linearSearch(value, array) {
-        for (let i = 0; i < array.length; i += 1) {
-            if (array[i] === value) return i;
-        }
+        for (let i = 0; i < array.length; i += 1) if (array[i] === value) return i;
+
+        return -1;
+    },
+    binarySearch: function binarySearch(value, array) {
+        let lowerBound = 0;
+        let upperBound = array.length;
+
+        do {
+            const midPoint = Math.floor(lowerBound + (upperBound - lowerBound) / 2);
+            const midValue = array[midPoint];
+
+            if (value > midValue) lowerBound = midPoint + 1;
+            else if (value < midValue) upperBound = midPoint - 1;
+            else if (midValue === value) return midPoint;
+        } while (lowerBound < upperBound);
 
         return -1;
     }
