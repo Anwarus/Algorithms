4717987a5060f63a329a6b8a1abbd536b48a1b30
Anwarus <Anwarus1289@gmail.com>
2019-03-15 21:34:06 +0100

    (feature) - bubbleSort and tests

--- js/algorithms.js
+++ js/algorithms.js
@@ -5,19 +5,54 @@ const algorithm = {
         return -1;
     },
     binarySearch: function binarySearch(value, array) {
-        let lowerBound = 0;
-        let upperBound = array.length;
+        let lb = 0;
+        let ub = array.length - 1;
 
         do {
-            const midPoint = Math.floor(lowerBound + (upperBound - lowerBound) / 2);
+            const midPoint = Math.floor(lb + (ub - lb) / 2);
             const midValue = array[midPoint];
 
-            if (value > midValue) lowerBound = midPoint + 1;
-            else if (value < midValue) upperBound = midPoint - 1;
+            if (midValue < value) lb = midPoint + 1;
+            else if (midValue > value) ub = midPoint - 1;
             else if (midValue === value) return midPoint;
-        } while (lowerBound < upperBound);
+        } while (lb <= ub);
 
         return -1;
+    },
+    interpolationSearch: function interpolationSearch(value, array) {
+        let lb = 0;
+        let ub = array.length - 1;
+
+        while (lb < ub && value >= array[lb] && value <= array[ub]) {
+            const pos =
+                lb + Math.floor(((ub - lb) / (array[ub] - array[lb])) * (value - array[lb]));
+            const posValue = array[pos];
+
+            if (posValue < value) lb = pos + 1;
+            else if (posValue > value) ub = pos - 1;
+            else if (posValue === value) return pos;
+        }
+
+        return -1;
+    },
+    bubbleSort: function bubbleSort(array) {
+        const clonedArray = array.slice(0);
+        let swapped = true;
+
+        while (swapped) {
+            swapped = false;
+
+            for (let i = 0; i < clonedArray.length - 1; i += 1) {
+                if (clonedArray[i] > clonedArray[i + 1]) {
+                    const tmp = clonedArray[i];
+                    clonedArray[i] = clonedArray[i + 1];
+                    clonedArray[i + 1] = tmp;
+                    swapped = true;
+                }
+            }
+        }
+
+        return clonedArray;
     }
 };
 
--- test/test.js
+++ test/test.js
@@ -33,6 +33,24 @@ describe('test linearSearch', function test() {
 });
 
 describe('test binarySearch', function test() {
+    it('should return -1 when value is not in array', function testCase() {
+        const value = 10;
+        const array = [1, 2, 3, 4];
+
+        const result = algorithm.binarySearch(value, array);
+
+        result.should.equal(-1);
+    });
+
+    it('should return -1 when array is empty', function testCase() {
+        const value = 10;
+        const array = [];
+
+        const result = algorithm.binarySearch(value, array);
+
+        result.should.equal(-1);
+    });
+
     it('should return 3 when value is on 3th index', function testCase() {
         const value = 10;
         const array = [1, 4, 6, 10, 15];
@@ -40,5 +58,51 @@ describe('test binarySearch', function test() {
         const result = algorithm.binarySearch(value, array);
 
         result.should.equal(3);
+    });
+});
+
+describe('test interpolationSearch', function test() {
+    it('should return -1 when value is not in array', function testCase() {
+        const value = 10;
+        const array = [1, 2, 3, 4];
+
+        const result = algorithm.interpolationSearch(value, array);
+
+        result.should.equal(-1);
+    });
+
+    it('should return -1 when array is empty', function testCase() {
+        const value = 10;
+        const array = [];
+
+        const result = algorithm.interpolationSearch(value, array);
+
+        result.should.equal(-1);
+    });
+
+    it('should return 4 when value is on 4th index', function testCase() {
+        const value = 10;
+        const array = [1, 2, 4, 6, 10, 14];
+
+        const result = algorithm.interpolationSearch(value, array);
+
+        result.should.equal(4);
+    });
+});
+
+describe('test bubbleSort', function test() {
+    it('should return sorted array', function testCase() {
+        const array = [12, 10, 4, 5];
+
+        const result = algorithm.bubbleSort(array);
+
+        expect([4, 5, 10, 12]).to.eql(result);
+    });
+    it('should return empty array on empty array given', function testCase() {
+        const array = [];
+
+        const result = algorithm.bubbleSort(array);
+
+        expect(array).to.eql(result);
     });
 });
